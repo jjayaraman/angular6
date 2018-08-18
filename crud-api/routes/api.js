f9d057eb2f78100e054d6ae70e2473d9645b456d
Jayakumar Jayaraman <india.jai@gmail.com>
2018-08-18 19:25:34 +0100

    more code added

--- crud-api/routes/api.js
+++ crud-api/routes/api.js
@@ -1,6 +1,24 @@
-//import express from 'express';
 const express = require('express');
 const router = express.Router();
+
+const MongoClient = require('mongodb').MongoClient;
+
+const URL = 'mongodb://localhost:27017';
+const DATABASE_NAME = 'crud';
+var DB;
+
+var options = { useNewUrlParser: true };
+
+MongoClient.connect(URL, options, (error, client) => {
+    if (error) {
+        console.log('DB Connection error: ' + error);
+        return;
+    }
+    //console.log(client);
+    DB = client.db(DATABASE_NAME);
+    console.log('DB connected..');
+})
+
 
 
 router.get('/', (req, res) => {
@@ -8,7 +26,18 @@ router.get('/', (req, res) => {
 })
 
 router.get('/employees', (req, res) => {
-    res.send('hello..');
+
+    console.log('DB : ' + DB);
+    const employee = DB.collection('employee');
+    employee.find({}).toArray((error, docs) => {
+        if (error) {
+            console.log('Error : ' + error);
+        }
+        console.log('docs :  ' + JSON.stringify(docs));
+        res.send(docs);
+    })
 })
+
+
 
 module.exports = router;
