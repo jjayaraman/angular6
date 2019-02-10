1071fa1a909a89126e9de3b018f0728b67b39d85
Jayakumar Jayaraman <india.jai@gmail.com>
2019-02-10 13:02:05 +0000

    updated

--- crud-api/routes/api.js
+++ crud-api/routes/api.js
@@ -5,17 +5,18 @@ const MongoClient = require('mongodb').MongoClient;
 
 const URL = 'mongodb://localhost:27017';
 const DATABASE_NAME = 'crud';
-var DB;
+var db;
 
 var options = { useNewUrlParser: true };
 
+// DB connect once, and use many times
 MongoClient.connect(URL, options, (error, client) => {
     if (error) {
         console.log('DB Connection error: ' + error);
         return;
     }
     //console.log(client);
-    DB = client.db(DATABASE_NAME);
+    db = client.db(DATABASE_NAME);
     console.log('DB connected..');
 })
 
@@ -27,13 +28,13 @@ router.get('/', (req, res) => {
 
 router.get('/employees', (req, res) => {
 
-    console.log('DB : ' + DB);
-    const employee = DB.collection('employee');
+    const employee = db.collection('employee');
+    
     employee.find({}).toArray((error, docs) => {
         if (error) {
             console.log('Error : ' + error);
+            res.send(error);
         }
-        console.log('docs :  ' + JSON.stringify(docs));
         res.send(docs);
     })
 })
